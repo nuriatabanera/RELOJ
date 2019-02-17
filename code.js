e408da0cfd36d49b3574a9f21debfff641781f9e
nuriatabanera <nurss.ts@gmail.com>
2019-02-17 21:18:33 +0100

    version2

--- code.js
+++ code.js
@@ -1,7 +1,18 @@
 //document.ready
 
-$(function () {
-	// body...
+$(function(){
+	
+	function bucle(){
+		let tiempo = new Date();
+		let hora = (tiempo.getHours() < 10 ? '0'+tiempo.getHours() : tiempo.getHours());
+		let minuto = (tiempo.getMinutes() < 10 ? '0'+tiempo.getMinutes() : tiempo.getMinutes());
+		let segundo = (tiempo.getSeconds() < 10 ? '0'+tiempo.getSeconds() : tiempo.getSeconds());
+		
+		$("#reloj").html(`${hora}:${minuto}:${segundo}`);
+		
+		setTimeout(function(){ bucle(); },1000);
+	}
+	bucle();
 
-	$("#reloj").html(new Date);
-})
+
+});
